0d7ed20e98bb039619beb1080aacf74fee36af49
muminuljava <muminul.tab@yahoo.com>
2024-09-04 12:39:36 +0600

    App.java

--- App.java
+++ App.java
@@ -1,5 +1,10 @@
+//Created By Muminul Islam
+
+
 public class App {
+    //Common variable
     public static void main(String[] args) throws Exception {
-        
+        //Star the GUI
+        Gui mainGUI=new Gui();
     }
 }
