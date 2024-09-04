f769decc2c3f0a4e359e387c2701ae0de5c3f8fe
muminuljava <muminul.tab@yahoo.com>
2024-09-04 12:40:01 +0600

    Client.java

--- Client.java
+++ Client.java
@@ -1,18 +1,27 @@
-/*
+/*Created By Muminul Islam
  * This Client program have two job
  * job one: get local ip and port
  * job two: make a client connection using this ip and port
  */
+import java.io.IOException;
 import java.net.Socket;
 
 public class Client{
-    Socket clinetSocket;
+    Socket clientSocket;
 
-    String ClientLocalIp;
-    int clientLocalSocketNumber;
+    String RemoteIp;
+    int remotePortNumber;
     
     public Client(){
-        clinetSocket=new Socket();
+
+        //Initialize Client
+        try {
+            clientSocket=new Socket(RemoteIp, remotePortNumber );
+            System.out.println("Connected to "+RemoteIp);
+        } catch (IOException ex) {
+            System.out.println("Client Ip FAILED!");
+        }
+
 
     }
 
